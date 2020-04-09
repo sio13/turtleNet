6cc82ded6f310cd0da60265033750f6a1a0e3164
martindatag <Martin.Gano1@cityfootball.com>
2020-04-09 19:31:34 +0200

    srg

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from keras.datasets import mnist
 from keras.models import load_model
 from keras.utils import to_categorical
 
-from cleverhans.attacks import ProjectedGradientDescent, FastGradientDescent, BasicIterativeMethod
+from cleverhans.attacks import ProjectedGradientDescent, FastGradientMethod, BasicIterativeMethod
 import cleverhans
 import numpy as np
 
