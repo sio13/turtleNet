0b0d0d81de7929b92b0f5b723015af058b484668
martindatag <Martin.Gano1@cityfootball.com>
2020-04-09 15:39:02 +0200

    added ta

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from cleverhans.attacks import FastGradientMethod
 import cleverhans
 import numpy as np
 
-def main():
+
+def main1():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = (x_train / 255).reshape((len(x_train), 28, 28, 1))
-
 
     attack = Attack(FastGradientMethod, 0.3, 0, 1)
 
@@ -22,10 +22,18 @@ def main():
 
     pert = attack.generate_perturbations(np.array(x_train), net.model, 6)
     print("adv data")
-    print(net.model.evaluate(pert.reshape(-1,28,28,1), to_categorical(y_train)))
+    print(net.model.evaluate(pert.reshape(-1, 28, 28, 1), to_categorical(y_train)))
 
 
+def main2():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = (x_train / 255).reshape((len(x_train), 28, 28, 1))
+
+    model = load_model("models/conv_nn.h5")
+
+    net = TurtleNet(model, FastGradientMethod, 0.3, 0, 1)
+    net.eval_on_attack(FastGradientMethod, 0.3, 0, 1, x_train, y_train)
 
 
 if __name__ == '__main__':
-    main()
+    main2()
