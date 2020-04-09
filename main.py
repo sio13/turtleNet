80d9d5f4fa82bedc9bfcf7c5d2700ccf4a11ec93
martindatag <Martin.Gano1@cityfootball.com>
2020-04-09 16:00:48 +0200

    dg

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main1():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = (x_train / 255).reshape((len(x_train), 28, 28, 1))
 
-    attack = Attack(FastGradientMethod, 0.3, 0, 1)
+    attack = Attack(BasicIterativeMethod, 0.3, 0, 1)
 
     net = CNNModel()
     net.train_on_mnist()
