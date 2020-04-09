5636d96405f1228b8fe69e0f23f4400cd5014eb7
martindatag <Martin.Gano1@cityfootball.com>
2020-04-09 16:00:06 +0200

    sgd

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ def main2():
 
     model = load_model("models/conv_nn.h5")
 
-    net = TurtleNet(model, FastGradientMethod, 0.3, 0, 1)
-    net.eval_on_attack(FastGradientMethod, 0.3, 0, 1, x_train, y_train)
+    net = TurtleNet(model, BasicIterativeMethod, 0.3, 0, 1)
+    net.eval_on_attack(BasicIterativeMethod, 0.3, 0, 1, x_train, y_train)
 
     print(net.model.evaluate(x_train, to_categorical(y_train)))
 
