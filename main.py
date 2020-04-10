0ea8090926db09a9d03d87ace93e561cd90e85e5
martindatag <Martin.Gano1@cityfootball.com>
2020-04-10 21:34:02 +0200

    sdg

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main2():
     net = TurtleNet(network.model, ProjectedGradientDescent, 0.3, 0, 1)
 
     net.adversarial_training(iterations=200, x_train=x_train, y_train=y_train, chunk_size=50,
-                             epochs_per_iteration=3, batch_size=50)
+                             epochs_per_iteration=1, batch_size=50)
 
     net.eval_on_attack(ProjectedGradientDescent, 0.3, 0, 1, x_test, y_test, chunk_size=50)
     net.eval_on_attack(FastGradientMethod, 0.3, 0, 1, x_test, y_test, chunk_size=50)
