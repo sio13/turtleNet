2bfa3af4996e1a957843d0d2c8a9a198b25dff8b
martindatag <Martin.Gano1@cityfootball.com>
2020-04-11 17:32:47 +0200

    zdg

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ def main3():
                 num_chunks=1,
                 save_to_file=True,
                 results_file_path="results/test.json",
+                folder_name="models",
                 folder_list=["checkpoint_0", "checkpoint_500", "checkpoint_1000", "checkpoint_1200"],
                 prefix="checkpoint_")
 
