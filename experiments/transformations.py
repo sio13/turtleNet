38dd11105ce0dd78a1826ab00f196ab185815742
martindatag <Martin.Gano1@cityfootball.com>
2020-04-12 12:48:42 +0200

    added experiments

---
+++ experiments/transformations.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+def main():
+
+    pictures = '../pictures'
+
+    df_all_models = pd.read_csv(open("../results/all_models.csv"))
+    attacks = list(df_all_models.attack.unique())
+    for attack in attacks:
+        df_specific_attack = df_all_models.loc[df_all_models['attack'] == attack]
+
+        df_specific_attack.sort_values(by=['iteration']).plot(x='iteration', y='accuracy').figure.savefig(
+            f"{pictures}/{attack}_accuracy_by_iterations.png")
+
+        df_specific_attack.sort_values(by=['attack_time']).plot(x='attack_time', y='accuracy').figure.savefig(
+            f"{pictures}/{attack}_accuracy_by_attack_time.png")
+
+        df_specific_attack.sort_values(by=['iteration']).plot(x='iteration', y='loss').figure.savefig(
+            f"{pictures}/{attack}_loss_by_iterations.png")
+
+        df_specific_attack.sort_values(by=['attack_time']).plot(x='attack_time', y='loss').figure.savefig(
+            f"{pictures}/{attack}_loss_by_attack_time.png")
+
+
+
+
+
+if __name__ == '__main__':
+    main()
