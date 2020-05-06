07fdef88471de0a38e1d517302a782b350b6b045
martindatag <Martin.Gano1@cityfootball.com>
2020-05-06 21:02:39 +0200

    fixed import

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import attack
 from train import TurtleNet
 
 from target_model import CNNModel
-from keras.datasets import cifar100
+from keras.datasets import cifar10
 from keras.models import load_model
 from keras.utils import to_categorical
 
