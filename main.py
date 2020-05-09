add38d30bd82d91465de4270110c78a61ec87f45
martindatag <Martin.Gano1@cityfootball.com>
2020-05-09 11:21:13 +0200

    fixed

--- main.py
+++ main.py
@@ -158,11 +158,12 @@ def main7():
 
 def train_cifar10_robust():
 
-    network = CNNModel()
+    #network = CNNModel()
+    model = load_model("models/resnet_raw.h5")
     x_train, y_train, x_test, y_test = get_keras_dataset(
         cifar10.load_data(), input_shape=(-1, 32, 32, 3))
 
-    net = TurtleNet(network.model,
+    net = TurtleNet(model,
                     ProjectedGradientDescent,
                     0.3)
 
