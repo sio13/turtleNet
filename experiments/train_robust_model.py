1c31c9bc613f5b592017c28a386c328bee265a85
martindatag <Martin.Gano1@cityfootball.com>
2020-05-20 17:57:20 +0200

    fixed experimetn

--- experiments/train_robust_model.py
+++ experiments/train_robust_model.py
@@ -66,10 +66,10 @@ if __name__ == '__main__':
     train_model(model=target_model.model,
                 dataset=get_keras_dataset(cifar10.load_data(), input_shape=(-1, 32, 32, 3)),
                 iteration_total=15000,
-                checkpoint_dir='../models_cifar_better_new',
+                checkpoint_dir='../models_cifar_better',
                 epsilon=0.1,
                 iteration_so_far=0,
                 attack_type=ProjectedGradientDescent,
                 use_natural=True,
-                frequency_natural=5
+                frequency_natural=2
                 )
